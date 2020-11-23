193a4d0b74eb2633c4e5bf76ecfb1d1530571766
DeboDevelop <debajyotidofficial@gmail.com>
2020-11-23 22:37:25 +0530

    Removed useless codes.

--- socket.js
+++ socket.js
@@ -10,19 +10,19 @@ module.exports = function(server) {
 			let roomUsers = getCurrentRoomUsers(roomName);
 			socket.join(user.roomName);
 
-			console.log(`${user.username} has joined ${user.roomName}`);
+			//console.log(`${user.username} has joined ${user.roomName}`);
 			io.sockets
 				.to(user.roomName)
 				.emit("roomEntry", { username, roomUsers });
 
 			// Broadcast when user sends message
 			socket.on("message", data => {
-				console.log(data);
+				//console.log(data);
 				socket.broadcast.to(user.roomName).emit("message", data);
 			});
 
 			socket.on("disconnect", () => {
-				console.log(`${user.username} has left ${user.roomName}`);
+				//console.log(`${user.username} has left ${user.roomName}`);
 				setUserLeftRoom(user.id);
 				roomUsers = getCurrentRoomUsers(roomName);
 				socket.broadcast
@@ -31,23 +31,4 @@ module.exports = function(server) {
 			});
 		});
 	});
-
-	// const pythonRoom = io.of("/python-room");
-	// // on new connection
-	// pythonRoom.on("connection", socket => {
-	// 	console.log("new connection to python room: ", socket.id);
-	// 	socket.join("python");
-
-	// 	// receive new message
-	// 	socket.on("message", data => {
-	// 		// send to everyone except for the sender
-	// 		console.log(data);
-	// 		// io.to("python").emit("message", data);
-	// 		socket.broadcast.to("python").emit("message", data);
-	// 	});
-
-	// 	socket.disconnect("disconnect", () => {
-	// 		console.log(`disconnected: ${socket.id}"`);
-	// 	});
-	// });
 };
